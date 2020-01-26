2a239e7bf0681486639aa8229c4ebab8616b10aa
ituski <tf.k.itsuki.rits@ezweb.ne.jp>
2020-01-26 13:55:17 +0900

    bearer token

--- functions/index.js
+++ functions/index.js
@@ -24,7 +24,42 @@ firebase.initializeApp(firebaseConfig);
 
 const db = admin.firestore();
 
-app.get("/screams", (req, res) => {
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+    console.log(idToken);
+  } else {
+    console.error("No token found");
+    return res.status(403).json({ error: "Unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.error("Error while verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+
+app.get("/screams", FBAuth, (req, res) => {
   db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
@@ -43,19 +78,19 @@ app.get("/screams", (req, res) => {
     .catch(err => console.error(err));
 });
 
-app.post("/scream", (req, res) => {
-  //   if (req.method !== "POST") {
-  //     return res.status(400).json({ error: "Method not allowed" });
-  //   }
+app.post("/scream", FBAuth, (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ error: "Body must not be empty" });
+  }
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
   db.collection("screams")
     .add(newScream)
     .then(doc => {
-      res.json({ message: `document ${doc} created successfully ` });
+      res.json({ message: `document ${doc.id} created successfully ` });
     })
     .catch(err => {
       res.status(500).json({ error: `something went wrong` });
