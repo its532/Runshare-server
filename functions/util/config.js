73ad1666ea8d50612497a8dfe127436898220065
ituski <tf.k.itsuki.rits@ezweb.ne.jp>
2020-01-28 03:35:10 +0900

    image upload

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBoqtOLNk-JDbtvD0AaW2XkguFQNxDMivE",
+  authDomain: "socialape-5ae20.firebaseapp.com",
+  databaseURL: "https://socialape-5ae20.firebaseio.com",
+  projectId: "socialape-5ae20",
+  storageBucket: "socialape-5ae20.appspot.com",
+  messagingSenderId: "63375322242",
+  appId: "1:63375322242:web:0b1aa620e5bc944468589b",
+  measurementId: "G-080378YMQJ"
+};
