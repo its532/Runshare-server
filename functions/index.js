738c8b948ddee43b1ee865adfa30ea158d2e1c4e
ituski <tf.k.itsuki.rits@ezweb.ne.jp>
2020-01-29 03:08:56 +0900

    userdetail add

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,13 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 // var serviceAccount = require("./serviceAccountKey.json");
 // admin.initializeApp({
@@ -20,5 +26,7 @@ app.post("/scream", FBAuth, postOneScream);
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
